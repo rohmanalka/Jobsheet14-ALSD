4028332584abcd7a23211e971075585b0db51485
rohmanalka <rohmanalka06@gmail.com>
2024-06-09 21:04:38 +0700

    update

--- GraphMain21.java
+++ GraphMain21.java
@@ -1,41 +1,82 @@
 import java.util.Scanner;
-
 public class GraphMain21 {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        Graph21 gedung = new Graph21(6);
-        gedung.addEdge(0, 1, 50);
-        gedung.addEdge(0, 2, 100);
-        gedung.addEdge(1, 3, 70);
-        gedung.addEdge(2, 3, 40);
-        gedung.addEdge(3, 4, 60);
-        gedung.addEdge(4, 5, 80);
-        gedung.degree(0);
-        gedung.printGraph();
-        gedung.removeEdge(1, 3);
-        gedung.printGraph();
-
-        System.out.print("Masukkan gedung asal: ");
-        int asal5 = scanner.nextInt();
-        System.out.print("Masukkan gedung tujuan: ");
-        int tujuan5 = scanner.nextInt();
-        if (gedung.isAdjacent(asal5, tujuan5)) {
-            System.out.println("Gedung " + (char)('A' + asal5) + " dan Gedung " + (char)('A' + tujuan5) + " bertetangga");
-        } else {
-            System.out.println("Gedung " + (char)('A' + asal5) + " dan Gedung " + (char)('A' + tujuan5) + " tidak bertetangga");
-        }
-        System.out.println("");
-
-        GraphMatriks21 gdg = new GraphMatriks21(4);
-        gdg.makeEdge(0, 1, 50);
-        gdg.makeEdge(1, 0, 60);
-        gdg.makeEdge(1, 2, 70);
-        gdg.makeEdge(2, 1, 80);
-        gdg.makeEdge(2, 3, 40);
-        gdg.makeEdge(3, 0, 90);
-        gdg.printGraph();
-        System.out.println("Hasil setelah penghapusan edge");
-        gdg.removeEdge(2, 1);
-        gdg.printGraph();
+        System.out.print("Masukkan jumlah vertex: ");
+        int v = scanner.nextInt();
+        Graph21 graph = new Graph21(v); 
+        
+        int pil;
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Add Edge");
+            System.out.println("2. Remove Edge");
+            System.out.println("3. Degree");
+            System.out.println("4. Print Graph");
+            System.out.println("5. Cek Edge");
+            System.out.println("6. Update Jarak");
+            System.out.println("7. Hitung Edge");
+            System.out.println("0. Exit");
+            System.out.print("Masukkan pilihan: ");
+            pil = scanner.nextInt();
+            
+            switch (pil) {
+                case 1:
+                    System.out.print("Masukkan gedung asal: ");
+                    int asal1 = scanner.nextInt();
+                    System.out.print("Masukkan gedung tujuan: ");
+                    int tujuan1 = scanner.nextInt();
+                    System.out.print("Masukkan jarak: ");
+                    int jarak1 = scanner.nextInt();
+                    graph.addEdge(asal1, tujuan1, jarak1);
+                    break;
+                case 2:
+                    System.out.print("Masukkan gedung asal: ");
+                    int asal2 = scanner.nextInt();
+                    System.out.print("Masukkan gedung tujuan: ");
+                    int tujuan2 = scanner.nextInt();
+                    graph.removeEdge(asal2, tujuan2);
+                    break;
+                case 3:
+                    System.out.print("Masukkan gedung: ");
+                    int gedung = scanner.nextInt();
+                    graph.degree(gedung);
+                    break;
+                case 4:
+                    graph.printGraph();
+                    break;
+                case 5:
+                    System.out.print("Masukkan gedung asal: ");
+                    int asal5 = scanner.nextInt();
+                    System.out.print("Masukkan gedung tujuan: ");
+                    int tujuan5 = scanner.nextInt();
+                    if (graph.isAdjacent(asal5, tujuan5)) {
+                        System.out.println("Gedung " + (char)('A' + asal5) + " dan Gedung " + (char)('A' + tujuan5) + " bertetangga");
+                    } else {
+                        System.out.println("Gedung " + (char)('A' + asal5) + " dan Gedung " + (char)('A' + tujuan5) + " tidak bertetangga");
+                    }
+                    break;
+                case 6:
+                    System.out.print("Masukkan gedung asal: ");
+                    int asal6 = scanner.nextInt();
+                    System.out.print("Masukkan gedung tujuan: ");
+                    int tujuan6 = scanner.nextInt();
+                    System.out.print("Masukkan jarak baru: ");
+                    int jarak6 = scanner.nextInt();
+                    graph.updateJarak(asal6, tujuan6, jarak6);
+                    break;
+                case 7:
+                    int edgeCount = graph.hitungEdge();
+                    System.out.println("Jumlah edge dalam graf: " + edgeCount);
+                    break;
+                case 0:
+                    System.out.println("Program selesai.");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan masukkan pilihan yang benar.");
+            }
+        } while (pil != 0);
+        
+        scanner.close();
     }
 }

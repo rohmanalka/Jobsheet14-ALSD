bb34787d330e93643af8d6d227a71dd7869155d2
rohmanalka <rohmanalka06@gmail.com>
2024-06-09 20:54:42 +0700

    update

--- GraphMain21.java
+++ GraphMain21.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class GraphMain21 {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         Graph21 gedung = new Graph21(6);
         gedung.addEdge(0, 1, 50);
         gedung.addEdge(0, 2, 100);
@@ -11,6 +14,17 @@ public class GraphMain21 {
         gedung.printGraph();
         gedung.removeEdge(1, 3);
         gedung.printGraph();
+
+        System.out.print("Masukkan gedung asal: ");
+        int asal5 = scanner.nextInt();
+        System.out.print("Masukkan gedung tujuan: ");
+        int tujuan5 = scanner.nextInt();
+        if (gedung.isAdjacent(asal5, tujuan5)) {
+            System.out.println("Gedung " + (char)('A' + asal5) + " dan Gedung " + (char)('A' + tujuan5) + " bertetangga");
+        } else {
+            System.out.println("Gedung " + (char)('A' + asal5) + " dan Gedung " + (char)('A' + tujuan5) + " tidak bertetangga");
+        }
+        System.out.println("");
 
         GraphMatriks21 gdg = new GraphMatriks21(4);
         gdg.makeEdge(0, 1, 50);
